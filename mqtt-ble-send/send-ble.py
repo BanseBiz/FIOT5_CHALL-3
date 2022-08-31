243c4f58b58dc2947c379047e5df6912b6c50eb9
Marvin Banse <marvin@banse.biz>
2022-08-31 16:38:24 +0200

    test on/off

--- mqtt-ble-send/send-ble.py
+++ mqtt-ble-send/send-ble.py
@@ -1,11 +1,14 @@
 import asyncio
+import time
+
 from bleak import BleakClient
 from bleak import BleakClient, BleakScanner
 from bleak.exc import BleakError
 
 ADDRESS = "ff:21:09:28:22:5a"
-CHARACTERISTIC = "0xfffe"
-
+CHARACTERISTIC = "0xfff3"
+ON = "0xBC01010155"
+OFF = "0xBC01010055"
 
 
 async def main():
@@ -13,6 +16,9 @@ async def main():
     if not device:
         raise BleakError(f"A device with address {ADDRESS} could not be found.")
     async with BleakClient(device) as client:
+        await client.write_gatt_char(CHARACTERISTIC, OFF)
+        time.sleep(2.0)
+        await client.write_gatt_char(CHARACTERISTIC, ON)
         svcs = await client.get_services()
         print("Services:")
         for service in svcs:
