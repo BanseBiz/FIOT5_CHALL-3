fc6079c133998635d67c6cde8ff9afd522d00dd7
Marvin Banse <marvin@banse.biz>
2022-08-31 10:25:10 +0200

    fix commandline IR-Command script

--- mgtt-ir-send/send-ir.py
+++ mgtt-ir-send/send-ir.py
@@ -2,20 +2,23 @@ import paho.mqtt.client as paho
 import sys
 import time
 
-broker="192.168.0.234"
+broker="192.168.0.235"
 port=1883
 
-topic="cmnd/tasmota_23DFCF"
-
+topic="cmnd/tasmota_23DFCF/IRsend"
+head="{\"Protocol\":\"NEC\",\"Bits\":"
+head2="\",\"Data\":"
 
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
     pass
 
-for cmnd in sys.argv[1:] :
+for data in sys.argv[1:] :
+    bits = len(data)*4
+    msg = head + "\"" + str(bits) + head2 + data + "\"}"
     client1 = paho.Client("control1")                           #create client object
     client1.on_publish = on_publish                          #assign function to callback
     client1.connect(broker,port)                                 #establish connection
-    ret = client1.publish(topic,cmnd)
-    print(cmnd)
-    time.sleep(0.8)
+    ret = client1.publish(topic,msg)
+    print(msg)
+    time.sleep(0.2)
