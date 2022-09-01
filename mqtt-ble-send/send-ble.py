520476d8c1e738c8043db86d9a35a967c9340e2c
Marvin Banse <marvin@banse.biz>
2022-09-01 11:16:40 +0200

    send right data format

--- mqtt-ble-send/send-ble.py
+++ mqtt-ble-send/send-ble.py
@@ -7,9 +7,8 @@ from bleak.exc import BleakError
 
 ADDRESS = "ff:21:09:28:32:6c"
 CHARACTERISTIC = "0000fff3-0000-1000-8000-00805f9b34fb"
-ON = "0xBC01010155"
-OFF = "0xBC01010055"
-
+ON = "\xbc01010155"
+OFF = [0xbc,0x01,0x01,0x00,0x55]
 
 async def main():
     device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)
@@ -17,16 +16,15 @@ async def main():
         raise BleakError(f"A device with address {ADDRESS} could not be found.")
     async with BleakClient(device) as client:
         svcs = await client.get_services()
-        print("Services:")
-        for service in svcs:
-            print(service)
-            for char in service.characteristics:
-                print("Characteristic:")
-                print(char)
-        await client.write_gatt_char(CHARACTERISTIC, OFF)
-        time.sleep(2.0)
-        await client.write_gatt_char(CHARACTERISTIC, ON)
-
+#        print("Services:")
+#        for service in svcs:
+#            print(service)
+#            for char in service.characteristics:
+#                print("Characteristic:")
+#                print(char)
+        byte_data = bytearray(OFF)
+        await client.write_gatt_char(CHARACTERISTIC, byte_data)
+        print(f"send {OFF}")
 
 if __name__ == "__main__":
     asyncio.run(main())
