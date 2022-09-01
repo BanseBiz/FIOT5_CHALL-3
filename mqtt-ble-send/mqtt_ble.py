4d863784803d45803b67d811a64f577bc0729cf7
Marvin Banse <marvin@banse.biz>
2022-09-01 21:05:44 +0200

    set condif-file for bridge

--- mqtt-ble-send/mqtt_ble.py
+++ mqtt-ble-send/mqtt_ble.py
@@ -9,14 +9,9 @@ from bleak import BleakClient
 from bleak import BleakClient, BleakScanner
 from bleak.exc import BleakError
 
-ADDRESS = "ff:21:09:28:32:6c"
+#ADDRESS = "ff:21:09:28:32:6c"
+ADDRESS = "ff:21:09:28:22:5a"
 CHARACTERISTIC = "0000fff3-0000-1000-8000-00805f9b34fb"
-ON = "0xBC01010155"
-OFF = "0xBC01010055"
-
-MQTT_TOPIC = "cmnd/ble"
-MQTT_HOST = '192.168.0.152'
-MQTT_PORT = 1883
 
 ble_commands = []
 
@@ -25,8 +20,8 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    print("Message received-> " + msg.topic + " " + str(msg.payload))
-    ble_commands.append(msg.payload)
+    print("Message received-> " + msg.topic + " " + str(msg.payload.decode("utf-8")))
+    ble_commands.append(msg.payload.decode("utf-8"))
 
 def ble_command_thread():
     asyncio.run(ble_command_handle())
@@ -38,29 +33,41 @@ async def ble_command_handle():
                 ble_commands_list = json.loads(f.read())
                 f.close()
                 print("Connecting to ble device")
-                try: 
-                    device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)
-                    if not device:
-                        print(f"a device with address {ADDRESS} could not be found.")
-                    async with BleakClient(device) as client:
-                        await client.write_gatt_char(CHARACTERISTIC, ble_commands_list[ble_commands.pop(0)])
-                        print(f"send command to ble device")
-                except:
-                    print("An exception occurred")
-
+                with open('mqtt_ble_config.json') as conf_f:
+                    config = json.loads(conf_f.read())
+                    address = config["BLE_ADDRESS"]
+                    characteristic = config["CHARACTERISTIC"]
+                    try: 
+                        device = await BleakScanner.find_device_by_address(address, timeout=20.0)
+                        if not device:
+                            print(f"a device with address {address} could not be found.")
+                        async with BleakClient(device) as client:
+                            await client.write_gatt_char(characteristic, bytearray.fromhex(ble_commands_list[ble_commands.pop(0)]))
+                            print(f"send command to ble device")
+                        conf_f.close()
+                    except:
+                        print("Ohh no, an exception occurred. That's sad")
         await asyncio.sleep(2.0)
     return 1
 
 
 async def main():   
-    ble_thread = threading.Thread(target=ble_command_thread)
-    ble_thread.start()
-    client = mqtt.Client("mqtt_ble_bridge")
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(MQTT_HOST, MQTT_PORT)
-    client.subscribe(MQTT_TOPIC,0)
-    client.loop_forever()
+    with open('mqtt_ble_config.json') as conf_f:
+        config = json.loads(conf_f.read())
+        address = config["BLE_ADDRESS"]
+        topic = config["MQTT_LISTEN_TOPIC"]
+        mqtt_host = config["MQTT_HOST"]
+        mqtt_port = int(config["MQTT_PORT"])
+        conf_f.close()    
+
+        ble_thread = threading.Thread(target=ble_command_thread)
+        ble_thread.start()
+        client = mqtt.Client("mqtt_ble_bridge")
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(mqtt_host, mqtt_port)
+        client.subscribe(topic,0)
+        client.loop_forever()
 
 if __name__ == "__main__":
     asyncio.run(main())
