11f069274e2ed079dd5b008787c8f644d72bf34b
Marvin Banse <marvin@banse.biz>
2022-09-01 16:38:39 +0200

    interpret json

--- mqtt-ble-send/send-ble.py
+++ mqtt-ble-send/send-ble.py
@@ -1,5 +1,7 @@
 import asyncio
 import time
+import json
+import sys
 
 from bleak import BleakClient
 from bleak import BleakClient, BleakScanner
@@ -10,21 +12,17 @@ CHARACTERISTIC = "0000fff3-0000-1000-8000-00805f9b34fb"
 ON = "bc01010155"
 OFF = "bc01010055"
 
-
 async def main():
-    device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)
+    with open('jtx_commands.json') as f:
+        d = json.loads(f.read())
+        f.close()
+    device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)    
     if not device:
         raise BleakError(f"A device with address {ADDRESS} could not be found.")
-    async with BleakClient(device) as client:
-        svcs = await client.get_services()
-#        print("Services:")
-#        for service in svcs:
-#            print(service)
-#            for char in service.characteristics:
-#                print("Characteristic:")
-#                print(char)
-        await client.write_gatt_char(CHARACTERISTIC, bytearray.fromhex(OFF))
-        print(f"send {OFF}")
+    for cmnd in sys.argv[1:] :
+        async with BleakClient(device) as client:
+            await client.write_gatt_char(CHARACTERISTIC, bytearray.fromhex(d[cmnd]))
+        print(f"send {cmnd}")
 
 if __name__ == "__main__":
     asyncio.run(main())
