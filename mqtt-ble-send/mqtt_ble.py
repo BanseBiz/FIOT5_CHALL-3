6162a960f045abda19e505914dad548dfb3ada03
Marvin Banse <marvin@banse.biz>
2022-09-01 14:55:15 +0200

    ble-thread

--- mqtt-ble-send/mqtt_ble.py
+++ mqtt-ble-send/mqtt_ble.py
@@ -26,21 +26,27 @@ def on_message(client, userdata, msg):
     print("Message received-> " + msg.topic + " " + str(msg.payload))
     ble_commands.append(msg.payload)
 
-async def main():
-    device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)
-    client = mqtt.Client("mqtt_ble_bridge")
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(MQTT_HOST, MQTT_PORT)
-    client.subscribe(MQTT_TOPIC,0)
+async def ble_command_handle():
     while(true)
         if ble_commands
+            print("Connectin to ble device")
             device = await BleakScanner.find_device_by_address(ADDRESS, timeout=20.0)
             if not device:
                 raise BleakError(f"A device with address {ADDRESS} could not be found.")
             async with BleakClient(device) as client:
                 await client.write_gatt_char(CHARACTERISTIC, bytearray.fromhex(ble_commands.pop(0)))
-        time.sleep(2.0)
+        await asyncio.sleep(2.0)
+    return 1
+
+
+async def main():
+    ble_thread = threading.Thread(target=ble_command_handle)
+    ble_thread.start()
+    client = mqtt.Client("mqtt_ble_bridge")
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(MQTT_HOST, MQTT_PORT)
+    client.subscribe(MQTT_TOPIC,0)
     client.loop_forever()
 
 if __name__ == "__main__":
