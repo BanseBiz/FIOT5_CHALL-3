d85ed6bebf2d1f34c0b0d5ba8c981f996091f9db
evilscientist <suryansh@evilscientist.cc>
2022-09-02 11:00:05 +0200

    Update button logic

--- newApp.py
+++ newApp.py
@@ -19,7 +19,7 @@ head="{\"Protocol\":\"NEC\",\"Bits\":"
 head2="\",\"Data\":"
 
 count_sent=1
-STATE_CLOCK = False
+STATE_CLOCK=False
 
 def on_publish(client,userdata,result):
     #create function for callback
@@ -131,6 +131,9 @@ def update_output(on, value, n_clicks):
     ########TURN-OFF/ON#####
 #print(value)
     #print(n_clicks)
+    print(STATE_CLOCK)
+    print(on)
+
 
     if on == True:
         msgclock = "0xFFA25D"  # POWER ON
@@ -141,10 +144,6 @@ def update_output(on, value, n_clicks):
         print(msg1)
         STATE_CLOCK = True
         time.sleep(1.0)
-
-    print(on)
-    print(STATE_CLOCK)    
-
 
     if  STATE_CLOCK == True and on == False:
         msgclock = "0xFFA25D"  # POWER ON
