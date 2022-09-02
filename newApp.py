1915c7f4ae63818cce086e6560d5460a05c22b4a
evilscientist <suryansh@evilscientist.cc>
2022-09-02 09:44:51 +0200

    CSS update 13

--- newApp.py
+++ newApp.py
@@ -100,7 +100,7 @@ html.Div(id='container-button-timestamp'),
     ),
 #    html.Button('Turn on/off', id='turn-off', n_clicks=0),
 
-    dcc.Input(id='username', value='Time', type='text', style={'width': "80", 'padding': "1em"}),
+    dcc.Input(id='username', value='Time', type='text', style={'width': "80", 'padding': "1em", "align-items": "center"}),
     ###DMX###
 html.Button('Submit', id='submit-val', n_clicks=0, style={"margin-left": "15px"}),
 
